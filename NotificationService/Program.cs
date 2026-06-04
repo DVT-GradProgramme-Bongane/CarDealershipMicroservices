a5153ea6c8bddaf9dca0cd325dd45be155b06320
Your Name <NRankapole@dvtsoftware.com>
2026-06-04 10:07:03 +0200

    feat: Create connection string + register db context

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -1,5 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using CarDealerShipMicroService.NotificationService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var host = builder.Configuration["POSTGRES_HOST"]
+           ?? throw new InvalidOperationException("POSTGRES_HOST is not configured.");
+
+var port = builder.Configuration["POSTGRES_PORT"]
+           ?? throw new InvalidOperationException("POSTGRES_PORT is not configured.");
+
+var database = builder.Configuration["POSTGRES_DB"]
+               ?? throw new InvalidOperationException("POSTGRES_DB is not configured.");
+
+var user = builder.Configuration["POSTGRES_USER"]
+           ?? throw new InvalidOperationException("POSTGRES_USER is not configured.");
+
+var password = builder.Configuration["POSTGRES_PASSWORD"]
+               ?? throw new InvalidOperationException("POSTGRES_PASSWORD is not configured.");
+
+var connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password}";
+
+builder.Services.AddDbContext<NotificationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db_context = scope.ServiceProvider.GetRequiredService<NotificationDbContext>();
+    db_context.Database.EnsureCreated();
+}
 
 app.Run();
