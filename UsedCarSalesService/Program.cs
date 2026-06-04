1117dc163e755dfcb6ace532b159d14f00a106db
Carina Diesel <cdiesel@dvtsoftware.com>
2026-06-04 09:39:01 +0200

    chore: Remove exposed username and password

--- UsedCarSalesService/Program.cs
+++ UsedCarSalesService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using UsedCarSalesService.Data;
 using UsedCarSalesService.Services;
 
@@ -6,9 +7,40 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-var connectionString =
+var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER")
+    ?? throw new InvalidOperationException("POSTGRES_USER is required.");
+var dbPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")
+    ?? throw new InvalidOperationException("POSTGRES_PASSWORD is required.");
+var dbHost = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+var dbPort = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+
+var baseConnectionString =
     builder.Configuration.GetConnectionString("UsedCarSalesDb")
-    ?? "Host=localhost;Port=5432;Database=used_car_sales;Username=postgres;Password=postgres";
+    ?? "Host=localhost;Port=5432;Database=used_car_sales";
+
+var connectionBuilder = new NpgsqlConnectionStringBuilder(baseConnectionString)
+{
+    Username = dbUser,
+    Password = dbPassword
+};
+
+if (!string.IsNullOrWhiteSpace(dbHost))
+{
+    connectionBuilder.Host = dbHost;
+}
+
+if (int.TryParse(dbPort, out var parsedPort))
+{
+    connectionBuilder.Port = parsedPort;
+}
+
+if (!string.IsNullOrWhiteSpace(dbName))
+{
+    connectionBuilder.Database = dbName;
+}
+
+var connectionString = connectionBuilder.ConnectionString;
 
 builder.Services.AddDbContext<UsedCarSalesDbContext>(options =>
     options.UseNpgsql(connectionString));
