b910bca00b46c27782d68a0a62425a1d829388ab
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-06-08 13:58:42 +0200

    fix: add CORS headers to api gateway

--- ApiGateway/server.js
+++ ApiGateway/server.js
@@ -3,6 +3,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 const routes = {
     inventory: process.env.INVENTORY_SERVICE_URL,
     staff: process.env.STAFF_SERVICE_URL,
