8166cf62738e3e7d2215b6da8997273fc55a9ccd
Carina Diesel <cdiesel@dvtsoftware.com>
2026-06-08 13:42:40 +0200

    chore: Make table reusable

--- Frontend/car-dealership/src/components/SalesTable.tsx
+++ Frontend/car-dealership/src/components/SalesTable.tsx
@@ -20,51 +20,6 @@ export interface SalesTransaction {
   createdAt: Date;
 }
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
-
 export async function SalesTable() {
   const response = await fetch(`http://localhost:5005/used-sales`);
 
@@ -88,13 +43,13 @@ export async function SalesTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="">{invoice.totalAmount}</TableCell>
-            <TableCell>NA</TableCell>
+        {sales.map((sales) => (
+          <TableRow key={sales.id}>
+            <TableCell className="font-medium">{sales.carId}</TableCell>
+            <TableCell>{sales.clientId}</TableCell>
+            <TableCell>{sales.staffId}</TableCell>
+            <TableCell className="">{sales.salePrice}</TableCell>
+            <TableCell>{sales.status}</TableCell>
           </TableRow>
         ))}
       </TableBody>
